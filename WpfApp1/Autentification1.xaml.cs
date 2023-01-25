453cc88025c28b25dad83e36bf102437b454efb5
ДрейИА <@211-05>
2023-01-25 18:07:18 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Autentification1.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Autentification1.xaml
+    /// </summary>
+    public partial class Autentification1 : Window
+    {
+        static bool IsBlocedState = false;
+        DispatcherTimer timer = new();
+        public Autentification1()
+        {
+            InitializeComponent();
+            timer.Interval = new TimeSpan(0, 0, 10);
+            timer.Tick += new EventHandler(BackToMainWindow);
+            timer.Start();
+        }
+
+        private void BackToMainWindow(object sender, EventArgs e)
+        {
+            MessageBox.Show("Время вышло ");
+            IsBlocedState = true;
+            DialogResult = IsBlocedState;
+            Close();
+        }
+
+        private void FiveSignsBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FiveSignsBox.Text.Length < 5)
+            {
+                return;
+            }
+            if (MainWindow.GlobalNumbers == FiveSignsBox.Text)
+            {
+                MessageBox.Show("все норм");
+                IsBlocedState = false;
+                DialogResult = IsBlocedState;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Введенные значения не совпадпют с ранее продемонстрированными данными");
+                IsBlocedState = true;
+                DialogResult = IsBlocedState;
+                Close();
+            }
+
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DispatcherTimer timer = new();
+        private readonly DispatcherTimer dispatcherTimer = new();
+        public static string GlobalNumbers;
+        int time = 0;
+
+        
+        private void setVisibility()
+        {
+            StaticBlocedString.Visibility = Visibility.Visible;
+            EditingBlocedString.Visibility = Visibility.Visible;
+        }
+
+        private void setUsability(object sender, EventArgs e)
+        {
+            
+        }
+        private void setNewText(object sender, EventArgs e)
+        {
+            EditingBlocedString.Text = time++
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void SartAutorization_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new();
+            GlobalNumbers = random.Next(10000, 100000).ToString();
+            MessageBoxResult result;
+            result = MessageBox.Show(GlobalNumbers);
+            if (result != 0)
+            {
+                Autentification1 taskWindow = new()
+                {
+                    Owner = this
+                };
+                bool? d = taskWindow.ShowDialog();
+                if (d.HasValue && d.Value)
+                {
+                    setVisibility();
+                    timer.Interval = new TimeSpan(0, 0, 10);
+                    timer.Tick += new EventHandler(setUsability);
+                    timer.Start();
+
+                    
+                    dispatcherTimer.Tick += new EventHandler();
+                    dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+                    dispatcherTimer.Start();
+                }
+
+            }
+            
+        }
+    }
+}
